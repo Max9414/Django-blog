fc365aaff661eda23ea55f62bd9ae4d85c565c65
Max94 <maxy_14@hotmail.it>
2024-05-21 10:57:37 +0000

    computer issues, sending everything to git before resetting

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import About
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(About)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'created_on')
+    summernote_fields = ('body',)
 
 # Register your models here.
--- about/models.py
+++ about/models.py
@@ -7,3 +7,6 @@ class About(models.Model):
     title = models.CharField(max_length=200, unique=True)
     body = models.TextField
     created_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"(self.title)"
